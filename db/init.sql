a3aaa8de917d6f64d826176ba87c1ab943c872bf
Paul Becker <p.becker@schrempp-edv.de>
2025-03-25 08:57:23 +0100

    setup-environment

--- db/init.sql
+++ db/init.sql
@@ -1,11 +1,11 @@
-
-CREATE TABLE IF NOT EXISTS eurusd_s10 (
-    id SERIAL PRIMARY KEY,
-    timestamp TIMESTAMPTZ NOT NULL,
-    open DOUBLE PRECISION,
-    high DOUBLE PRECISION,
-    low DOUBLE PRECISION,
-    close DOUBLE PRECISION,
-    volume DOUBLE PRECISION
+CREATE TABLE eurusd_s10 (
+  timestamp TIMESTAMPTZ NOT NULL,
+  open DOUBLE PRECISION,
+  high DOUBLE PRECISION,
+  low DOUBLE PRECISION,
+  close DOUBLE PRECISION,
+  volume DOUBLE PRECISION,
+  PRIMARY KEY (timestamp)
 );
+
 SELECT create_hypertable('eurusd_s10', 'timestamp', if_not_exists => TRUE);
